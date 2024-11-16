e4bd034f99bd8ba017d6b8c5af8b75c12fae186d
Spna Rai <er.sapnarai27@gmail.com>
2024-11-16 09:24:53 +0530

    Ep-05: Hooks

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const REST_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://assets.isu.pub/document-structure/230212034237-008e265485a452aa3b6f928768529512/v1/620d340e145dd891370752dd41a9d730.jpeg";
