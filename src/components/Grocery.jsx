9f7086d1129d214ea6396e701c01bc950dab06d8
Spna Rai <er.sapnarai27@gmail.com>
2025-04-01 13:39:14 +0530

    Ep-09: Performance Optimization

---
+++ src/components/Grocery.jsx
@@ -0,0 +1,9 @@
+const Grocery = () =>{
+    return (
+        <div>
+            <h1>Let's assume this is grocery delivery page and it has alot of components</h1>
+        </div>
+    )
+}
+
+export default Grocery;
