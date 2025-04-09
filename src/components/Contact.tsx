8915e6c392cb928b04c3e31f103cdc3ff550ca48
Sapna Rai <38311415+sapnarai27@users.noreply.github.com>
2025-04-09 21:33:12 +0530

    Revert "Revert "Ep-13: Testing Set Up""

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Contact = () => {
     return(
-        <div>
-            <h1>Contact Us</h1>
-            <h2>emial: abcd23@gmail.com 
-                <br></br>
-                contact: 9876543210
-            </h2>
+        <div className='p-5'>
+            <h1 className="font-bold text-2xl m-5">Contact Us</h1>
+            Name: <input type="text" className="border border-black p-2 m-5"/> 
+            Email: <input type="text" className="border border-black p-2 m-5"/> 
+            <button className="border-b-black rounded-md bg-gray-500 text-white p-2 cursor-pointer">Submit</button>
+
         </div>
     )
 }
